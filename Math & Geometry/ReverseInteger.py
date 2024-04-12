177a1270125194a53189c05fc4a3e6a0578ea108
Shaik Faizan Roshan Ali <roshan.shaik.ml@gmail.com>
2024-04-12 19:02:32 +0530

    Update ReverseInteger.py

--- Math & Geometry/ReverseInteger.py
+++ Math & Geometry/ReverseInteger.py
@@ -1,4 +1,8 @@
 """
+LeetCode [7]: Reverse Integer
+
+Given a signed 32-bit integer 'x', return 'x' with its digits reversed. If reversing 'x' causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+
 Approach:
 1. Define constants for the maximum and minimum integer values allowed.
 2. Initialize variables to store the result, flag for negative numbers, and handle the input if it's negative.
