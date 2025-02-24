a8b79ca455e821abac2720b7eed5fe3e17198125
Shaik Faizan Roshan Ali <roshan.shaik.ml@gmail.com>
2025-02-24 09:03:55 +0530

    Update KClosestsPointsToOrigin.py

--- Graphs/KClosestsPointsToOrigin.py
+++ Graphs/KClosestsPointsToOrigin.py
@@ -15,3 +15,14 @@ class Solution:
     
     def distance_squared(self, point: List[int]) -> int:
         return point[0]**2 + point[1]**2
+
+
+class SolutionOptimized:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        
+        for point in points:
+            distance = point[0] ** 2 + point[1] ** 2
+            heappush(minHeap, (distance, point))
+        
+        return [heappop(minHeap)[1] for _ in range(k)]
